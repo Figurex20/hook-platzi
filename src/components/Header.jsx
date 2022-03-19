bfea7a65bf9171429599843cd55a769e62a2bf68
FIGUREX <kennethggm12@gmail.com>
2022-03-18 23:32:56 -0600

    I finished the maquetacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Header.css';
 
 function Header() {
 	return (
 		<div className="Header">
-			<h1 className="Header-title">Platzi Conf Merch</h1>
-			<div className="Header-checkout">Checkout</div>
+			<h1 className="Header-title">
+				<Link to="/">Platzi Conf Merch</Link>
+			</h1>
+			<div className="Header-checkout">
+				<Link to="/checkout">
+					<i className="fas fa-shopping-basket" title="Checkout" />
+				</Link>
+			</div>
 		</div>
 	);
 }
